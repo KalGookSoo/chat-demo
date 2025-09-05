1e1605c31d1ee6783e0e7a5845776d93bd421bf8
kimdoyeob <ga.miro3721@gmail.com>
2025-09-05 10:03:46 +0900

    feat: 예외 처리 및 JPA 감사 설정 추가

---
+++ src/main/java/kr/me/seesaw/config/JpaAuditingConfig.java
@@ -0,0 +1,9 @@
+package kr.me.seesaw.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaAuditingConfig {
+}
